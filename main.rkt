d03c3597617aefea20b690493a04b964ec956b45
Subhajit Sahu <wolfram77@gmail.com>
2022-02-02 16:56:07 +0530

    :turtle: update name

--- main.rkt
+++ main.rkt
@@ -1,27 +1,27 @@
 #lang racket
 
-; (up.sexp sexp to)
+; (slist.up-sexp-to sexp to)
 ; removes a pair of parentheses from s-exp to list "to"
 ; : sexp=list -> (sexp .. to) (append)
 ; : else      -> (sexp . to)
-(define up.sexp
+(define slist.up-sexp-to
   (lambda (sexp to)
     (if (list? sexp)
         (append sexp to)
         (cons sexp to))))
 
-; (up.lst lst to): L X L -> L
+; (slist.up-to lst to): L X L -> L
 ; removes a pair of parentheses from each top-level element of lst to list "to"
 ; : lst=null -> to
-; : else     -> (up.sexp lst[0] (up.lst lst[1..end] to))
-(define up.lst
+; : else     -> (slist.up-sexp-to lst[0] (slist.up-to lst[1..end] to))
+(define slist.up-to
   (lambda (lst to)
     (if (null? lst)
         to
-        (up.sexp (car lst) (up.lst (cdr lst) to)))))
+        (slist.up-sexp-to (car lst) (slist.up-to (cdr lst) to)))))
 
-; (up lst): L -> L
+; (slist.up lst): L -> L
 ; removes a pair of parentheses from each top-level element of lst
-(define up
+(define slist.up
   (lambda (lst)
-    (up.lst lst (list))))
+    (slist.up-to lst (list))))
